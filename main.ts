52c51ed7c7828e1d6d565bcedd65f0615dc89e6a
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-10-12 16:44:38 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+let electrical_level = 0
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    electrical_level = pins.digitalReadPin(DigitalPin.P1)
+    pins.digitalWritePin(DigitalPin.P2, electrical_level)
 })
